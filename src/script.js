ea8df5cc4658e9b153ad1eeef69488a1ed85f45b
Marlene <marlenegarcia0908@gmail.com>
2023-05-09 21:48:45 -0300

    colocando material e textura

--- src/script.js
+++ src/script.js
@@ -22,12 +22,110 @@ const sizes = {
 // Scene
 const scene = new THREE.Scene()
 
-const material = new THREE.MeshBasicMaterial({ 
-    color: 0xff0000,
-})
+const loadingManager = new THREE.LoadingManager()
+loadingManager.onStart = () =>
+{
+    console.log('loading started')
+}
+loadingManager.onLoad = () =>
+{
+    console.log('loading finished')
+}
+loadingManager.onProgress = () =>
+{
+    console.log('loading progressing')
+}
+loadingManager.onError = () =>
+{
+    console.log('loading error')
+}
 
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+
+const colorTexture = textureLoader.load('/textures/minecraft.png')
+//const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png')
+//const colorTexture = textureLoader.load('/textures/door/color.jpg')
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+// colorTexture.rotation = Math.PI * 0.25
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+colorTexture.minFilter = THREE.NearestFilter
+
+
+colorTexture.magFilter = THREE.NearestFilter
 // Create an empty BufferGeometry
 const geometry = new THREE.BoxGeometry(1,1,1)
+
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/3/px.jpg',
+    '/textures/environmentMaps/3/nx.jpg',
+    '/textures/environmentMaps/3/py.jpg',
+    '/textures/environmentMaps/3/ny.jpg',
+    '/textures/environmentMaps/3/pz.jpg',
+    '/textures/environmentMaps/3/nz.jpg'
+])
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.5
+material.roughness = 0.1
+material.envMap = environmentMapTexture
+
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 64, 64),
+    material
+)
+sphere.position.x = - 1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1, 10, 100),
+    material
+)
+
+plane.position.y = -1.5
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 64, 128),
+    material
+)
+torus.position.x = 1.5
+
+
+scene.add(sphere, plane, torus)
+
+
 
 const PARAMS = {
     color: '#ff0000',
@@ -122,7 +220,7 @@ meshRotY.on('change', function(ev) {
     mesh.rotation.y = ev.value * Math.PI/180
 });
 
-folderMesh.addInput(mesh.material, "wireframe")
+//folderMesh.addInput(mesh.material, "wireframe")
 folderMesh.addInput(mesh, "visible")
 folderMesh.addInput(PARAMS, "color").on("change",(e)=>{
     material.color.set(new THREE.Color(e.value))
@@ -139,7 +237,7 @@ folderMesh.addButton({
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height,0.1,100)
-camera.position.z = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
